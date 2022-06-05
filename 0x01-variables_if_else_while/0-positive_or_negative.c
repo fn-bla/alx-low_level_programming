e2760ae3ec472ff6de6abb169befee7a137f003a
fn-bla <blaterengapna@gmail.com>
2022-06-05 18:22:51 -0400

    positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,12 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int n;
@@ -15,7 +21,7 @@ int main(void)
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("%d is zero\n", n);
+		printfk("%d is zero\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	return (0);
